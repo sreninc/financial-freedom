49b8fd647ac730563e2b93cf3e89031a81034d7a
sreninc <sreninc@gmail.com>
2021-03-20 13:14:09 +0000

    FEAT: Added functin body for contact form logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1191,5 +1191,26 @@ function loadResultsPage() {
 
 // Logic used to load the contact form and emailjs form handling on the about.html page
 function loadContactFormLogic() {
-    
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const templateParams = {
+            from_name: this.from_name.value,
+            email: this.email.value,
+            contact_type: this.contact_type.value,
+            message: this.message.value
+        };
+        emailjs.send('financial_freedom', 'financial_f_contact', templateParams)
+            .then(function(response) {
+                //console.log('SUCCESS!', response.status, response.text);
+                this.formSubmit.innerHTML = "Form Submitted";
+                this.formSubmit.classList.remove("btn-primary");
+                this.formSubmit.classList.add("btn-success");
+                this.formSubmit.classList.add("disabled");
+                this.formSubmit.type = "button";
+
+            }, function(error) {
+                //console.log('FAILED...', error);
+            });
+            this.reset();
+    });
 }
